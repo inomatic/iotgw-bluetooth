6513e1b945f784190cb3f27c8726f4b42bfaacb3
Alexander Brickwedde <alex@brickwedde.de>
2026-09-22 15:58:41 +0200

    debugging options

---
+++ iotgw-bluetooth/make_upload_etciotgw.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -Eeuo pipefail
+cd "$(dirname "$0")"
+
+IP=$1
+
+source /opt/dev-tools/oecore-x86_64/environment-setup-cortexa7t2hf-neon-vfpv4-oe-linux-gnueabi
+
+export BUILDVAR_GWBTNAME="Inomatic IoT-Gateway"
+export BUILDVAR_GWBTCONNECT="bluetooth/connect"
+export BUILDVAR_GWBTSERVICEUUID="6E400001-C352-11E5-953D-0002A5D5C51B"
+export BUILDVAR_GWBTRECEIVEACKUUID="6E400002-C352-11E5-953D-0002A5D5C51B"
+export BUILDVAR_GWBTTRANSMITUUID="6E400003-C352-11E5-953D-0002A5D5C51B"
+export BUILDVAR_GWBTRECEIVENOACKUUID="6E400004-C352-11E5-953D-0002A5D5C51B"
+export BUILDVAR_GWBTSTATUS="bluetooth/status"
+export BUILDVAR_GWBTBTGWFRAME="gateway/bluetoothframe"
+export BUILDVAR_GWBTBTBTFRAME="bluetooth/bluetoothframe"
+export BUILDVAR_GWBTCLIENTID="iotgw-bluetooth"
+export BUILDVAR_GWBTQUIT="global/quit"
+export BUILDVAR_GWBTMQTTHOST="127.0.0.1"
+export BUILDVAR_GWBTMQTTPORT=1884
+export BUILDVAR_GWBTMQTTUSER="iotgw-bluetooth"
+export BUILDVAR_GWBTMQTTPASSWORD="InoM4t1c_Passw0rd-FOr=Blu3t0th"
+make iotgw-bluetooth
+
+echo "Stopping iotgw-bluetooth"
+ssh -p 22 root@$IP "(systemctl stop iotgw-bluetooth ; killall -9 iotgw-bluetooth ; killall -9 gdbserver) || true"
+echo "Uploading iotgw-bluetooth"
+scp -P 22 iotgw-bluetooth root@$IP:/etc/iotgw/iotgw-bluetooth
+echo "Upload done."
